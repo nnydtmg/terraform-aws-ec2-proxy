138b854579c5ef4ab121b050aeb7a3a48b16e4fc
EC2 Default User <ec2-user@ip-172-31-81-15.ec2.internal>
2024-04-10 14:45:04 +0000

    Instance Connect Endpointに変更

--- ec2.tf
+++ ec2.tf
@@ -1,42 +1,13 @@
-# ------------------------------
-# IAM Role
-# ------------------------------
-# IAM Policy Document
-data "aws_iam_policy_document" "assumerole_ec2" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "ssm" {
-  name               = "ec2-ssm-role"
-  path               = "/"
-  assume_role_policy = data.aws_iam_policy_document.assumerole_ec2.json
+# ---------------------------
+# Instance Connect
+# ---------------------------
+resource "aws_ec2_instance_connect_endpoint" "main_eic" {
+  subnet_id          = aws_subnet.main_app_sn.id
+  security_group_ids = [aws_security_group.main_eic_sg.id]
+  preserve_client_ip = true
 
   tags = {
-    Roles = "ssm"
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "ssm" {
-  role       = aws_iam_role.ssm.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-# ------------------------------
-# Instance Profile
-# ------------------------------
-resource "aws_iam_instance_profile" "ssm" {
-  name = "ec2-ssm-role"
-  role = aws_iam_role.ssm.name
-
-  tags = {
-    Roles = "ssm"
+    Name = "terraform-eic"
   }
 }
 
@@ -52,26 +23,22 @@ resource "aws_instance" "app_ec2" {
   subnet_id                   = aws_subnet.main_app_sn.id
   associate_public_ip_address = "false"
 
-  iam_instance_profile = aws_iam_instance_profile.ssm.name
-
   tags = {
     Name = "terraform-app-ec2"
   }
 }
 
-# # Proxy EC2作成
-# resource "aws_instance" "proxy_ec2" {
-#   ami                         = "ami-051f8a213df8bc089"
-#   instance_type               = "t3.micro"
-#   availability_zone           = var.az_a
-#   vpc_security_group_ids      = [aws_security_group.proxy_ec2_sg.id]
-#   subnet_id                   = aws_subnet.main_proxy_sn.id
-#   associate_public_ip_address = "false"
-#   source_dest_check           = false
+# Proxy EC2作成
+resource "aws_instance" "proxy_ec2" {
+  ami                         = "ami-051f8a213df8bc089"
+  instance_type               = "t3.micro"
+  availability_zone           = var.az_a
+  vpc_security_group_ids      = [aws_security_group.proxy_ec2_sg.id]
+  subnet_id                   = aws_subnet.main_proxy_sn.id
+  associate_public_ip_address = "false"
+  source_dest_check           = false
 
-#   iam_instance_profile = aws_iam_instance_profile.ssm.name
-
-#   tags = {
-#     Name = "terraform-proxy-ec2"
-#   }
-# }
+  tags = {
+    Name = "terraform-proxy-ec2"
+  }
+}
--- vpc.tf
+++ vpc.tf
@@ -32,15 +32,15 @@ resource "aws_subnet" "main_app_sn" {
   }
 }
 
-# resource "aws_subnet" "main_proxy_sn" {
-#   vpc_id            = aws_vpc.main_vpc.id
-#   cidr_block        = "10.0.2.0/24"
-#   availability_zone = var.az_a
+resource "aws_subnet" "main_proxy_sn" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = var.az_a
 
-#   tags = {
-#     Name = "terraform-main-proxy-sn"
-#   }
-# }
+  tags = {
+    Name = "terraform-main-proxy-sn"
+  }
+}
 
 # ---------------------------
 # Internet Gateway
@@ -111,22 +111,22 @@ resource "aws_route_table_association" "main_app_rt_associate" {
 }
 
 # Route table作成(Proxy)
-# resource "aws_route_table" "main_proxy_rt" {
-#   vpc_id = aws_vpc.main_vpc.id
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_nat_gateway.main_ngw.id
-#   }
-#   tags = {
-#     Name = "terraform-main-proxy-rt"
-#   }
-# }
+resource "aws_route_table" "main_proxy_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.main_ngw.id
+  }
+  tags = {
+    Name = "terraform-main-proxy-rt"
+  }
+}
 
-# # SubnetとRoute tableの関連付け(App)
-# resource "aws_route_table_association" "main_proxy_rt_associate" {
-#   subnet_id      = aws_subnet.main_proxy_sn.id
-#   route_table_id = aws_route_table.main_proxy_rt.id
-# }
+# SubnetとRoute tableの関連付け(App)
+resource "aws_route_table_association" "main_proxy_rt_associate" {
+  subnet_id      = aws_subnet.main_proxy_sn.id
+  route_table_id = aws_route_table.main_proxy_rt.id
+}
 
 # ---------------------------
 # Security Group
@@ -140,6 +140,19 @@ resource "aws_security_group" "main_ec2_sg" {
     Name = "terraform-main-ec2-sg"
   }
 
+  # インバウンドルール
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.main_eic_sg.id]
+  }
   # アウトバウンドルール
   egress {
     from_port   = 0
@@ -150,26 +163,50 @@ resource "aws_security_group" "main_ec2_sg" {
 }
 
 # Security Group作成
-# resource "aws_security_group" "proxy_ec2_sg" {
-#   name        = "terraform-proxy-ec2-sg"
-#   description = "For Proxy EC2 Linux"
-#   vpc_id      = aws_vpc.main_vpc.id
-#   tags = {
-#     Name = "terraform-proxy-ec2-sg"
-#   }
+resource "aws_security_group" "proxy_ec2_sg" {
+  name        = "terraform-proxy-ec2-sg"
+  description = "For Proxy EC2 Linux"
+  vpc_id      = aws_vpc.main_vpc.id
+  tags = {
+    Name = "terraform-proxy-ec2-sg"
+  }
 
-#   # インバウンドルール
-#   ingress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["10.0.0.0/16"]
-#   }
-#   # アウトバウンドルール
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  # インバウンドルール
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.main_eic_sg.id]
+  }
+  # アウトバウンドルール
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Security Group作成
+resource "aws_security_group" "main_eic_sg" {
+  name        = "terraform-main-eic-sg"
+  description = "For Instance Connect"
+  vpc_id      = aws_vpc.main_vpc.id
+  tags = {
+    Name = "terraform-main-eic-sg"
+  }
+
+  # アウトバウンドルール
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+}
